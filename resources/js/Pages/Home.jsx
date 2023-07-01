6ffa01128a943003299644d306aa8310a4f0ce56
Qufel <97023524+Qufel@users.noreply.github.com>
2023-07-01 18:11:42 +0200

    Basic structure and sass

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import '../../sass/_tokens.scss';
+
+const Home = () => {
+    return (
+        <h1>This is test component</h1>
+    )
+}
+
+export default Home

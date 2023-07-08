3812d6e2126596bebc610d6c88c8e9fd5f072f72
loudsheep <loudsheep05@gmail.com>
2023-07-08 15:17:10 +0200

    Working login and register forms

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { Head, Link, useForm } from '@inertiajs/react';
 
-export default function Login() {
+export default function Login({ status }) {
+    // create empty form
+    const { data, setData, post, processing, errors, reset } = useForm({
+        email: '',
+        password: '',
+    });
+
+    // clear password filed on page rerender
+    useEffect(() => {
+        return () => {
+            reset('password');
+        };
+    }, []);
+
+    const handleOnChange = (event) => {
+        // TODO after adding Remember Me checkbox, add condition to pass either event.target.value or event.target.checked
+        setData(event.target.name, event.target.value);
+    };
+
+    // submit the form, do not reload, and send data to login controller
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route('login'));
+    };
+
     return (
         <>
-            LOGIN PAGE
+            <form onSubmit={submit}>
+                <input type="text" name="email" placeholder={"email"} onChange={handleOnChange} />    <br />
+                {errors.email} <br /><br />
+                <input type="password" name="password" placeholder={"password"} onChange={handleOnChange} /> <br />
+                {errors.password} <br /><br />
+
+                <input type="submit" value="Login" />
+            </form>
         </>
     )
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
-        <title>Confy</title>
-        @viteReactRefresh 
-        @vite(['resources/js/app.jsx'])
-        <!-- As you can see, we will use vite with jsx syntax for React-->
-        @inertiaHead
-    </head>
-    <body>
-        @inertia
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    <title>Confy</title>
+
+    @routes
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx'])
+    <!-- As you can see, we will use vite with jsx syntax for React-->
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
 </html>

fbea9971ab3e5f350284f300768d5cd20a25070a
loudsheep <loudsheep05@gmail.com>
2023-07-14 20:32:16 +0200

    Creating posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,28 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use Intervention\Image\Facades\Image;
 
 class PostController extends Controller
 {
-    public function index(Request $request) {
+    // show posts page
+    public function index(Request $request)
+    {
     }
 
-    public function create(Request $request) {
+    // display post creation page
+    public function create(Request $request)
+    {
+        return Inertia::render('Posts/CreatePost');
     }
 
-    public function store(Request $request) {
+    // store/save post to DB
+    public function store(Request $request)
+    {
+        $request->validate([
+            'body' => ["required", "string", "min:10", "max:500"],
+            'images' => ["array", "max:5"],
+            'images.*' => ["image", "mimes:jpg,png,bmp,gif", "max:5120"]
+        ]);
+
+        $post = $request->user()->posts()->create([
+            'body' => $request->body,
+        ]);
+
+        foreach ($request->images as $key => $value) {
+            if ($value == null) continue;
+
+            $path = $value->store('uploads', 'public');
+            $image = Image::make(public_path("storage/" . $path))->fit(500, 500);
+            $image->save();
+
+            $path = "/storage/" . $path;
+
+            $post->images()->create([
+                'image_url' => $path,
+            ]);
+        }
+
+        return redirect()->back();
     }
 
-    public function show(Request $request) {
+    // show post page
+    public function show(Request $request)
+    {
     }
 
-    public function edit(Request $request) {
+    // display edit view
+    public function edit(Request $request)
+    {
     }
 
-    public function update(Request $request) {
+    // save changed/edit
+    public function update(Request $request)
+    {
     }
 
-    public function destroy(Request $request) {
+    // delete post
+    public function destroy(Request $request)
+    {
     }
 }
---
+++ resources/js/Pages/Posts/CreatePost.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import { Head, Link, useForm } from '@inertiajs/react';
+
+export default function CreatePost({ status }) {
+    // create empty form
+    const { data, setData, post, processing, errors, reset } = useForm({
+        body: '',
+        images: [],
+    });
+
+    const handleOnChange = (event) => {
+        setData(event.target.name, event.target.value);
+    };
+
+    const handleImageUpload = (event) => {
+        setData("images", [...data.images, event.target.files[0]]);
+    };
+
+    const deleteImage = (index) => {
+        setData("images", data.images.filter((v, i) => {
+            return i !== index;
+        }));
+    }
+
+    // submit the form, do not reload, and send data to login controller
+    const submit = (e) => {
+        e.preventDefault();
+        post(route('post.store'));
+    };
+
+    return (
+        <>
+            <h1>Create New Post</h1>
+            <br />
+            <form onSubmit={submit}>
+                <textarea name="body" id="" cols="50" rows="20" placeholder='Post content' onChange={handleOnChange} value={data.body}></textarea><br />
+                {errors.email}
+                <br /><br />
+
+                <input type="file" name="image" id="" onChange={handleImageUpload} />
+                <br />
+
+                {data.images.map((value, index) => (
+                    <img src={URL.createObjectURL(value)} alt="" style={{ maxWidth: "100px", maxHeight: "100px", cursor: "pointer" }} onClick={() => deleteImage(index)} />
+                ))}
+
+                <br /><br />
+
+                <input type="submit" value="Create" />
+            </form>
+        </>
+    )
+}
